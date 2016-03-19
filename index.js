c5459860facd5851e7143db9c4b1db3fbed2eb08
James <james@mjsquare.com>
2016-03-19 16:48:41 +0000

    making some stuff and yeah you know

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 80;
 var data = {};
 
+function Bot (x,y) {
+    this.x = x;
+    this.y = y;
+}
 
 //Server goodness
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
-  data = {"bot":[{"x":800,"y":10},{"x":50,"y":50},{"x":20,"y":200}],"projectile":[{"x":70,"y":70}]};
 });
 
 //Routing
@@ -18,10 +21,25 @@ app.use('/', express.static('public'));
 //Socket Goodness
 io.on('connection', function(socket){
   console.log('A user has connected');
-  socket.on('code submission', function(code){
+  socket.on('code submission', code, function(){
     console.log('code submitted: ' + code);
+    addBot(randInt(0,999),randInt(0,499));
   });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
+
+function addBot(x,y) {
+  data.push(new Bot(x,y));
+}
+
+function updateBoardTick() {
+  io.emit('board update', data);  
+}
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+setInterval(updateBoardTick,1000);
--- public/main.js
+++ public/main.js
@@ -1,5 +1,34 @@
-$(function(){
-  //init sockets
-  var socket = io();
-  socket.emit('connection');
+var socket = io();
+socket.emit('connection');
+$('form').submit(function(){
+  socket.emit('code submission', $('#code-area').val());
+  return false;
 });
+function clearScreen(context) {
+    context.clearRect (0,0,1000,500);
+}
+function drawBot(x,y,context) {
+      context.fillRect(x,y,10,10);
+      context.stroke(); 
+}
+function drawProjectile(x,y,context) {
+      context.fillRect(x,y,2,2);
+      context.stroke(); 
+}
+context = document.getElementById('arena').getContext("2d");
+function updateGameState(data) {
+  clearScreen(context);
+  for (var i = 0; i < data["bot"].length; i++) {
+    var bot = data["bot"][i];
+    drawBot(bot["x"],bot["y"],context);
+  }
+  for (var i = 0; i < data["projectile"].length; i++) {
+    var projectile = data["projectile"][i];
+    drawProjectile(projectile["x"],projectile["y"],context);
+  }
+}
+
+socket.on('board update', function(jsonData){
+    updateGameState(jsonData);
+ });
+//setInterval(updateGameState,1000);
