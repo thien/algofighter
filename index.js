9dde981a69b80e3b34a2369d89a74b0bd0ff5642
James <james@mjsquare.com>
2016-03-19 15:21:16 +0000

    testing socket

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 80;
+var data = {};
+
 
 //Server goodness
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
+  data = {"bot":[{"x":800,"y":10},{"x":50,"y":50},{"x":20,"y":200}],"projectile":[{"x":70,"y":70}]};
 });
 
 //Routing
@@ -15,4 +18,10 @@ app.use('/', express.static('public'));
 //Socket Goodness
 io.on('connection', function(socket){
   console.log('A user has connected');
+  socket.on('code submission', function(code){
+    console.log('code submitted: ' + code);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
